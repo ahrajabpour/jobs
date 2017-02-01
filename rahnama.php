7013aa18789305b68dc6c728d5c488d97a7b1034
Amir <tokhobi67@gmail.com>
2017-02-01 10:30:22 +0330

    get link and image from specific page

--- rahnama.php
+++ rahnama.php
@@ -3,7 +3,6 @@
     require 'simple_html_dom/simple_html_dom.php';
     Requests::register_autoloader();
     // $response = Requests::get('http://www.rahnama.com/cat/index/id/37859/page/1/برنامه-نويس');
-    // http://www.rahnama.com/cat/index/id/37917/%D8%B7%D8%B1%D8%A7%D8%AD-(%D9%88%D8%A8-%D8%B3%D8%A7%D9%8A%D8%AA)
     $response = Requests::get('http://www.rahnama.com/cat/index/id/37917/%D8%B7%D8%B1%D8%A7%D8%AD-(%D9%88%D8%A8-%D8%B3%D8%A7%D9%8A%D8%AA)');
 
     $html = new simple_html_dom();
