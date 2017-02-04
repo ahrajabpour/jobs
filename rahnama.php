738ab57395b51ff656c00caa045c4f1971846acb
Amir <tokhobi67@gmail.com>
2017-02-04 21:38:39 +0330

    get total pages from rahnama in rahnama.php

--- rahnama.php
+++ rahnama.php
@@ -1,18 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>rahnama</title>
+    <style>
+        body{direction: rtl;}
+    </style>
+</head>
+<body>
+
+</body>
+</html>
 <?php 
     require 'Requests-1.7.0/library/Requests.php';
     require 'simple_html_dom/simple_html_dom.php';
     Requests::register_autoloader();
-    // $response = Requests::get('http://www.rahnama.com/cat/index/id/37859/page/1/برنامه-نويس');
+    $response = Requests::get('http://www.rahnama.com/cat/index/id/37859/page/1/برنامه-نويس');
     
-    $response = Requests::get('http://www.rahnama.com/cat/index/id/37917/%D8%B7%D8%B1%D8%A7%D8%AD-(%D9%88%D8%A8-%D8%B3%D8%A7%D9%8A%D8%AA)');
+    // $response = Requests::get('http://www.rahnama.com/cat/index/id/37917/%D8%B7%D8%B1%D8%A7%D8%AD-(%D9%88%D8%A8-%D8%B3%D8%A7%D9%8A%D8%AA)');
+
 
     $html = new simple_html_dom();
     $html->load_file($response->url);
 
-    foreach ($html->find('div#listings a') as $item){
-        if (!$item->attr['class'] == 'addtofav') {
-            echo $item;
-            echo "<hr>";
+    $pages = explode(':', $html->find('li.pager_last',0)->plaintext);
+
+    $i = 1;
+    while ($i <= intval($pages[1])) {
+        foreach ($html->find('div#listings a') as $item){
+            if (!$item->attr['class'] == 'addtofav') {
+                echo $item;
+                echo "<hr>";
+            }
         }
+        $i++;
+        sleep(30);
     }
  ?>
